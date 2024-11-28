60671aaca6408b08ffd061e90890c86fca149d31
Tatiana Nieminen <tatniem@gmail.com>
2024-11-28 19:47:55 +0200

    feedback app

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import * as feedbacks from "./feedbacks.js";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hi again!"));
+app.get("/feedbacks/:id", async (c) => {
+    const range = c.req.param("id");
+    const count = await feedbacks.getFeedbacks(range);
+    return c.text(`Feedback ${range}: (${count})`);
+});
+
+app.post("/feedbacks/:id", async (c) => {
+    const range = c.req.param("id");
+    await feedbacks.incrementFeedbacks(range);
+});
 
 export default app;
